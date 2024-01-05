6ee0ff2fc937848ea2d21d599e8c89b022b7056c
cxrala <ameliang123@gmail.com>
2024-01-05 19:18:16 +0000

    a lot of refactoring.

--- src/Simulation/Shallow.hs
+++ src/Simulation/Shallow.hs
@@ -1,143 +1,72 @@
-module Shallow where
+module Simulation.Shallow where
 
+import           Data.Ord
 import           Data.Vector         ((!))
 import qualified Data.Vector         as V
-import qualified Data.Vector.Mutable as MV
+import           Utils.Matrix
 
 -- for now, implemented almost identically to
 -- http://graphics.cs.cmu.edu/nsp/course/15-464/Fall09/papers/StamFluidforGames.pdf
--- matrix --
--- to change to single vector later? adapted from yampa-game-of-life
--- type Matrix a = V.Vector (V.Vector a)
--- matrixInitialise :: (Int, Int) -> a -> Matrix a
--- matrixInitialise (rows, cols) val = V.replicate rows (V.replicate cols val)
--- matrixGet :: (Int, Int) -> Matrix a -> a
--- matrixGet (x, y) m = (m ! x) ! y
-type Matrix a = V.Vector a
+type DensityField = Matrix Double
 
-flattenDims :: (Int, Int) -> Int -> Int
-flattenDims (x, y) width = x + width * y
+type VelocityFieldX = Matrix Double
 
-expandDims :: Int -> Int -> (Int, Int)
-expandDims i width = (i `mod` width, i `div` width)
-
-matrixInitialise :: (Int, Int) -> a -> Matrix a
-matrixInitialise (nrows, ncols) = V.replicate (nrows * ncols)
-
-matrixGet :: (Int, Int) -> Matrix a -> Int -> a
-matrixGet dims m width = m ! flattenDims dims width
-
---- canvas density ---
-data Canvas = Canvas
-  { densityField  :: Matrix Double
-  , velocityField :: Matrix (Double, Double) -- x, y
-  , canvasSize    :: (Int, Int) -- height, width
-  }
+type VelocityFieldY = Matrix Double
 
 type Source = Matrix Double
 
-defaultCanvas :: (Int, Int) -> Canvas
-defaultCanvas dimensionsNonPad@(x, y) =
-  Canvas
-    { densityField = matrixInitialise (x + 2, y + 2) 0
-    , velocityField = matrixInitialise (x + 2, y + 2) (0, 0)
-    , canvasSize = (x + 2, y + 2)
-    }
-
-withinBounds :: (Int, Int) -> Canvas -> Bool
-withinBounds (x, y) canvas =
-  x > 0
-    && y > 0
-    && x < fst (canvasSize canvas) - 1
-    && y < snd (canvasSize canvas) - 1
-
-addSource :: Canvas -> Source -> Double -> Canvas
-addSource canvas source dt =
-  let width = fst (canvasSize canvas)
-      newField =
-        V.imap
-          (\i n ->
-             let (idx, idy) = expandDims i width
-              in n + dt * matrixGet (idx, idy) source width)
-          $ densityField canvas
-   in canvas {densityField = newField}
-
-neighbours :: (Int, Int) -> Matrix a -> Canvas -> [a]
-neighbours (i, j) m canvas =
-  let width = fst (canvasSize canvas)
-   in map
-        (\(x, y) -> matrixGet (x, y) m width)
-        [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+addSource :: DensityField -> Source -> Double -> DensityField
+addSource densityField source dt =
+  matrixImap (\idxs dprev -> dprev + dt * matrixGet idxs source) densityField
 
 -- uses jacobi relaxation instead of gauss-seidel
 -- note: should probably change to gauss-seidel if you can figure out how to make it pure.
 -- why? convergence is faster (twice as fast) and also requires less storage.
-diffuse :: Canvas -> Int -> Double -> Double -> Canvas
-diffuse canvas b diff dt =
-  let width = fst (canvasSize canvas)
-      (h, w) = fromIntegralPair $ canvasSize canvas
-      a = dt * diff * h * w
-      x0 = densityField canvas
-      x = matrixInitialise (canvasSize canvas) 0
-      dif =
-        V.imap
-          (\i n ->
-             let (idx, idy) = expandDims i width
-              in if withinBounds (idx, idy) canvas
-                   then (matrixGet (idx, idy) x0 width
-                           + a * sum (neighbours (idx, idy) x canvas))
-                          / (1 + 4 * a)
-                   else n)
-   in canvas {densityField = iterate dif x !! 30}
+diffuse ::
+     Int
+  -> Int
+  -> DensityField
+  -> DensityField
+  -> Double
+  -> Double
+  -> DensityField
+diffuse n b x x0 diff dt =
+  let a = dt * diff * fromIntegral n * fromIntegral n
+      diffused =
+        matrixImapCheckbounds
+          (1, 1, n, n)
+          (\idxs dprev ->
+             (matrixGet idxs x0 + a * sum (matrixNeighbours idxs x))
+               / (1 + 4 * a))
+   in iterate diffused x !! 30
 
-advect :: Canvas -> Int -> Double -> Canvas
-advect canvas b dt =
-  let width = fst (canvasSize canvas)
-      dField = densityField canvas
-      vField = velocityField canvas
-      (n, m) = fromIntegralPair $ canvasSize canvas
-      dt0 = dt * n
-      advected =
-        V.imap
-          (\i val ->
-             let (idx, idy) = expandDims i width
-              in if withinBounds (idx, idy) canvas
-                   then let (vx, vy) = matrixGet (idx, idy) vField width
-                            lb = 0.5
-                            ubx = n + 0.5
-                            uby = m + 0.5
-                            x =
-                              let estimatedx = fromIntegral idx - dt0 * vx
-                               in if estimatedx < lb
-                                    then lb
-                                    else if estimatedx > n + ubx
-                                           then ubx
-                                           else estimatedx
-                            y =
-                              let estimatedy = fromIntegral idy - dt0 * vy
-                               in if estimatedy < lb
-                                    then lb
-                                    else if estimatedy > m + ubx
-                                           then ubx
-                                           else estimatedy
-                         in let i0 = floor x
-                                i1 = i0 + 1
-                                j0 = floor y
-                                j1 = j0 + 1
-                                s1 = x - fromIntegral i0
-                                s0 = 1 - s1
-                                t1 = y - fromIntegral j0
-                                t0 = 1 - t1
-                             in s0
-                                  * (t0 * matrixGet (i0, j0) dField width
-                                       + t1 * matrixGet (i0, j1) dField width)
-                                  + s1
-                                      * (t0 * matrixGet (i1, j0) dField width
-                                           + t1
-                                               * matrixGet (i1, j1) dField width)
-                   else val)
-          dField
-   in canvas {densityField = advected}
-
--- random util (move out later? or better way?) --
-fromIntegralPair = (\f (a, b) -> (f a, f b)) fromIntegral
+advect ::
+     Int
+  -> Int
+  -> DensityField
+  -> DensityField
+  -> VelocityFieldX
+  -> VelocityFieldY
+  -> Double
+  -> DensityField
+advect n b d d0 u v dt =
+  let dt0 = dt * fromIntegral n
+   in matrixImapCheckbounds
+        (1, 1, n, n)
+        (\idxs@(idx, idy) val ->
+           let lb = 0.5
+               ub = fromIntegral n + 0.5
+               x = clamp (lb, ub) $ fromIntegral idx - dt0 * matrixGet idxs u
+               y = clamp (lb, ub) $ fromIntegral idy - dt0 * matrixGet idxs v
+               i0 = floor x
+               i1 = i0 + 1
+               j0 = floor y
+               j1 = j0 + 1
+               s1 = x - fromIntegral i0
+               s0 = 1 - s1
+               t1 = y - fromIntegral j0
+               t0 = 1 - t1
+            in s0 * (t0 * matrixGet (i0, j1) d0 + t1 * matrixGet (i0, j1) d0)
+                 + s1
+                     * (t0 * matrixGet (i1, j0) d0 + t1 * matrixGet (i1, j1) d0))
+        d0
---
+++ src/Utils/Matrix.hs
@@ -0,0 +1,61 @@
+module Utils.Matrix
+  ( Matrix
+  , matrixInit
+  , matrixGet
+  , matrixMap
+  , matrixImap
+  , matrixImapCheckbounds
+  , matrixNeighbours
+  ) where
+
+import           Data.Tuple
+import           Data.Vector ((!))
+import qualified Data.Vector as V
+
+data Matrix a = Matrix
+  { vector :: V.Vector a
+  , dims   :: (Int, Int) -- height, width
+  }
+
+-- private
+mWidth :: Matrix a -> Int
+mWidth = snd . dims
+
+flattenDims :: (Int, Int) -> Matrix a -> Int
+flattenDims (x, y) m = x + mWidth m * y
+
+raiseDims :: Int -> Matrix a -> (Int, Int)
+raiseDims i m = quotRem i (mWidth m)
+
+-- exposed
+matrixInit :: (Int, Int) -> a -> Matrix a
+matrixInit dims@(rows, cols) val =
+  Matrix {vector = V.replicate rows val, dims = dims}
+
+matrixGet :: (Int, Int) -> Matrix a -> a
+matrixGet ij m = vector m ! flattenDims ij m
+
+matrixMap :: (a -> b) -> Matrix a -> Matrix b
+matrixMap f m = m {vector = V.map f (vector m)}
+
+matrixImap :: ((Int, Int) -> a -> b) -> Matrix a -> Matrix b
+matrixImap f m = m {vector = V.imap (\i -> f (raiseDims i m)) (vector m)}
+
+-- INCLUSIVE within bounds check.
+matrixImapCheckbounds ::
+     (Int, Int, Int, Int) -> ((Int, Int) -> a -> a) -> Matrix a -> Matrix a
+matrixImapCheckbounds bounds f m =
+  m
+    { vector =
+              V.imap (\i a ->
+              let point = raiseDims i m in
+              if withinBounds point bounds
+                then a
+                else f point a) (vector m)
+    }
+  where
+    withinBounds (x, y) (lbx, lby, ubx, uby) =
+      x >= lbx && y >= lby && x <= ubx && x <= uby
+
+matrixNeighbours :: (Int, Int) -> Matrix a -> [a]
+matrixNeighbours (i, j) m = map (`matrixGet` m) [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
