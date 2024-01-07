294c0f7b3e1385164b3756633308264f788e4d3a
cxrala <ameliang123@gmail.com>
2024-01-07 17:37:07 +0000

    finished backend implementation of fluid dynamics

--- src/Simulation/Shallow.hs
+++ src/Simulation/Shallow.hs
@@ -34,12 +34,13 @@ diffuse ::
 diffuse n b x x0 diff dt =
   let a = dt * diff * fromIntegral n * fromIntegral n
       diffused =
-        matrixImapCheckbounds
-          (1, 1, n, n)
-          (\idxs dprev ->
-             (matrixGet idxs x0 + a * sum (matrixNeighbours idxs x))
-               / (1 + 4 * a))
-   in iterate diffused x !! 30
+        matrixSetBnd n b
+          . matrixImapCheckbounds
+              (1, 1, n, n)
+              (\idxs dprev ->
+                 (matrixGet idxs x0 + a * sum (matrixNeighbours idxs x))
+                   / (1 + 4 * a))
+   in (iterate diffused x !! 30)
 
 advect ::
      Int
@@ -51,25 +52,78 @@ advect ::
   -> DensityField
 advect n b d0 u v dt =
   let dt0 = dt * fromIntegral n
-   in matrixImapCheckbounds
-        (1, 1, n, n)
-        (\idxs@(idx, idy) val ->
-           let lb = 0.5
-               ub = fromIntegral n + 0.5
-               x = clamp (lb, ub) $ fromIntegral idx - dt0 * matrixGet idxs u
-               y = clamp (lb, ub) $ fromIntegral idy - dt0 * matrixGet idxs v
-               i0 = floor x
-               i1 = i0 + 1
-               j0 = floor y
-               j1 = j0 + 1
-               s1 = x - fromIntegral i0
-               s0 = 1 - s1
-               t1 = y - fromIntegral j0
-               t0 = 1 - t1
-            in s0 * (t0 * matrixGet (i0, j0) d0 + t1 * matrixGet (i0, j1) d0)
-                 + s1
-                     * (t0 * matrixGet (i1, j0) d0 + t1 * matrixGet (i1, j1) d0))
-        d0
+      d =
+        matrixImapCheckbounds
+          (1, 1, n, n)
+          (\idxs@(idx, idy) val ->
+             let lb = 0.5
+                 ub = fromIntegral n + 0.5
+                 x = clamp (lb, ub) $ fromIntegral idx - dt0 * matrixGet idxs u
+                 y = clamp (lb, ub) $ fromIntegral idy - dt0 * matrixGet idxs v
+                 i0 = floor x
+                 i1 = i0 + 1
+                 j0 = floor y
+                 j1 = j0 + 1
+                 s1 = x - fromIntegral i0
+                 s0 = 1 - s1
+                 t1 = y - fromIntegral j0
+                 t0 = 1 - t1
+              in s0 * (t0 * matrixGet (i0, j0) d0 + t1 * matrixGet (i0, j1) d0)
+                   + s1
+                       * (t0 * matrixGet (i1, j0) d0
+                            + t1 * matrixGet (i1, j1) d0))
+          d0
+   in matrixSetBnd n b d
+
+project ::
+     Int -> VelocityFieldX -> VelocityFieldY -> (VelocityFieldX, VelocityFieldY)
+project n u v =
+  let h = 1 / fromIntegral n
+      div =
+        matrixSetBnd n 0
+          $ matrixImapCheckbounds
+              (1, 1, n, n)
+              (\(i, j) a ->
+                 -0.5
+                   * h
+                   * (matrixGet (i + 1, j) u
+                        - matrixGet (i - 1, j) u
+                        + matrixGet (i, j + 1) v
+                        - matrixGet (i, j - 1) v))
+              u
+      p = matrixInit (matrixDims u) 0
+      psolv =
+        let solvStep -- todo: lift linsolver out of functions diffuse and here
+             =
+              matrixSetBnd n 0
+                . matrixImapCheckbounds
+                    (1, 1, n, n)
+                    (\idxs dprev ->
+                       (matrixGet idxs div + sum (matrixNeighbours idxs p)) / 4)
+         in iterate solvStep p !! 30
+      newU =
+        matrixSetBnd n 1
+          $ matrixImapCheckbounds
+              (1, 1, n, n)
+              (\(i, j) uprev ->
+                 uprev
+                   - 0.5
+                       * (matrixGet (i + 1, j) psolv
+                            - matrixGet (i - 1, j) psolv)
+                       / h)
+              u
+      newV =
+        matrixSetBnd n 2
+          $ matrixImapCheckbounds
+              (1, 1, n, n)
+              (\(i, j) vprev ->
+                 vprev
+                   - 0.5
+                       * (matrixGet (i, j + 1) psolv
+                            - matrixGet (i, j - 1) psolv)
+                       / h)
+              v
+   in (newU, newV)
 
 -- x0 is initially the source vector
 -- x is the original density vector
@@ -92,3 +146,24 @@ densStep n x x0 u v diff dt =
         (let densAfterSource = addSource x x0 dt
              diffused = diffuse n 0 x0 densAfterSource diff dt
           in advect n 0 diffused u v dt)
+
+-- x0 is initially the source vector
+-- x is the original density vector
+velStep ::
+     Int
+  -> VelocityFieldX
+  -> VelocityFieldY
+  -> VelocityFieldX
+  -> VelocityFieldY
+  -> Double
+  -> Double
+  -> (VelocityFieldX, VelocityFieldY)
+velStep n u v u0 v0 visc dt =
+  let usrc = addSource u u0 dt
+      vsrc = addSource v v0 dt
+      diffusedu = diffuse n 1 u0 usrc visc dt
+      diffusedv = diffuse n 2 v0 vsrc visc dt
+      (uproj, vproj) = project n diffusedu diffusedv
+      advu = advect n 1 usrc diffusedu diffusedv dt
+      advv = advect n 1 vsrc diffusedu diffusedv dt
+   in project n advu advv
--- src/Utils/Matrix.hs
+++ src/Utils/Matrix.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Utils.Matrix
   ( Matrix
   , matrixDims
@@ -7,11 +9,16 @@ module Utils.Matrix
   , matrixImap
   , matrixImapCheckbounds
   , matrixNeighbours
+  , matrixUnsafeUpd
+  , matrixSetBnd
   ) where
 
+import           Control.Monad
+import           Control.Monad.ST
 import           Data.Tuple
-import           Data.Vector ((!))
-import qualified Data.Vector as V
+import           Data.Vector         ((!))
+import qualified Data.Vector         as V
+import qualified Data.Vector.Mutable as MV
 
 data Matrix a = Matrix
   { vector :: V.Vector a
@@ -51,15 +58,69 @@ matrixImapCheckbounds ::
 matrixImapCheckbounds bounds f m =
   m
     { vector =
-              V.imap (\i a ->
-              let point = raiseDims i m in
-              if withinBounds point bounds
-                then a
-                else f point a) (vector m)
+        V.imap
+          (\i a ->
+             let point = raiseDims i m
+              in if withinBounds point bounds
+                   then a
+                   else f point a)
+          (vector m)
     }
   where
     withinBounds (x, y) (lbx, lby, ubx, uby) =
       x >= lbx && y >= lby && x <= ubx && x <= uby
 
 matrixNeighbours :: (Int, Int) -> Matrix a -> [a]
-matrixNeighbours (i, j) m = map (`matrixGet` m) [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+matrixNeighbours (i, j) m =
+  map (`matrixGet` m) [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+
+matrixUnsafeUpd :: Matrix a -> [((Int, Int), a)] -> Matrix a
+matrixUnsafeUpd m ls =
+  m
+    { vector =
+        V.unsafeUpd (vector m) (map (\(dims, a) -> (flattenDims dims m, a)) ls)
+    }
+
+-- runs op on a vector, giving you a new vector. this is safe
+inPlace ::
+     forall a.
+     (forall s. MV.MVector (MV.PrimState (ST s)) a -> ST s ())
+  -> V.Vector a
+  -> V.Vector a
+inPlace op vec =
+  runST $ do
+    mv <- V.thaw vec
+    op mv
+    V.freeze mv
+
+-- this needs to be moved out of here...
+matrixSetBnd :: Int -> Int -> Matrix Double -> Matrix Double
+matrixSetBnd n b m =
+  let sbHelper mvec =
+        let mread dim1 dim2 = do
+              res1 <- MV.read mvec (flattenDims dim1 m)
+              res2 <- MV.read mvec (flattenDims dim2 m)
+              return $ 0.5 * res1 * res2
+            mwrite dims = MV.write mvec (flattenDims dims m)
+            setBoundVal dimReplace dim1 dim2 =
+              mwrite dimReplace =<< mread dim1 dim2
+            boundset prevDims bCheck neighbourDims =
+              MV.write
+                mvec
+                (flattenDims prevDims m)
+                (if b == bCheck
+                   then -1 * matrixGet neighbourDims m
+                   else matrixGet neighbourDims m)
+         in do
+              forM_
+                [1 .. n]
+                (\i -> do
+                   boundset (0, i) 1 (1, i)
+                   boundset (n + 1, i) 1 (n, i)
+                   boundset (i, 0) 2 (i, 1)
+                   boundset (i, n + 1) 2 (i, n))
+              setBoundVal (0, 0) (1, 0) (0, 1)
+              setBoundVal (0, n + 1) (1, n + 1) (0, n)
+              setBoundVal (n + 1, 1) (n, 0) (n + 1, 1)
+              setBoundVal (n + 1, n) (n, n + 1) (n + 1, n)
+   in m {vector = inPlace sbHelper (vector m)}
