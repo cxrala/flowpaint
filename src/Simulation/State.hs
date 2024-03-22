280dca7b91fccbb908f571672b89a1bb65de11c8
cxrala <ameliang123@gmail.com>
2024-03-22 15:59:04 +0000

    no memleak-vers

--- src/Simulation/State.hs
+++ src/Simulation/State.hs
@@ -20,104 +20,113 @@ import           Utils.Fields
 import           Utils.Matrix
 
 --- pigment layer, and its corresponding functions.
-data PigmentLayers = PigmentLayers
-  { surfaceLayerDensitiesRYB :: !(ScalarField, ScalarField, ScalarField)
-  , pigmentDensitiesRYB      :: !(ScalarField, ScalarField, ScalarField)
-  } deriving (Show)
+-- data PigmentLayers = PigmentLayers
+--   { surfaceLayerDensitiesRYB :: !(ScalarField, ScalarField, ScalarField)
+--   , pigmentDensitiesRYB      :: !(ScalarField, ScalarField, ScalarField)
+--   } deriving (Show)
 
-getColorLayer :: (PigmentLayers -> (a, a, a)) -> Color -> State -> a
-getColorLayer f color state =
-  let (r, y, b) = f $ pigLayers state
-   in case color of
-        Red    -> r
-        Yellow -> y
-        Blue   -> b
+-- getColorLayer :: (PigmentLayers -> (a, a, a)) -> Color -> State -> a
+-- getColorLayer f color state =
+--   let (r, y, b) = f $ pigLayers state
+--    in case color of
+--         Red    -> r
+--         Yellow -> y
+--         Blue   -> b
 
-getPigmentLayer :: Color -> State -> ScalarField
-getPigmentLayer = getColorLayer pigmentDensitiesRYB
+-- getPigmentLayer :: Color -> State -> ScalarField
+-- getPigmentLayer = getColorLayer pigmentDensitiesRYB
 
+-- getSurfaceLayer :: Color -> State -> ScalarField
+-- getSurfaceLayer = getColorLayer surfaceLayerDensitiesRYB
+
+-- updateColorLayer ::
+--      Color -> State -> ScalarField -> (ScalarField, ScalarField, ScalarField)
+-- updateColorLayer color state newVal =
+--   let (r, y, b) = surfaceLayerDensitiesRYB $ pigLayers state
+--    in case color of
+--         Red    -> (newVal, y, b)
+--         Yellow -> (r, newVal, b)
+--         Blue   -> (r, y, newVal)
+
+-- updateSurfaceLayer :: Color -> State -> ScalarField -> PigmentLayers
+-- updateSurfaceLayer color prevState newVal =
+--   let oldPigLayers = pigLayers prevState
+--    in oldPigLayers
+--         {surfaceLayerDensitiesRYB = updateColorLayer color prevState newVal}
+
+-- updatePigmentLayer :: Color -> State -> ScalarField -> PigmentLayers
+-- updatePigmentLayer color prevState newVal =
+--   let oldPigLayers = pigLayers prevState
+--    in oldPigLayers
+--         {pigmentDensitiesRYB = updateColorLayer color prevState newVal}
+
+-- stepPigmentLayers :: State -> Matrix Double -> Double -> PigmentLayers
+-- stepPigmentLayers state hmap dt =
+--   let pigLayer x = getPigmentLayer x state
+--       surfaceLayer x = getSurfaceLayer x state
+--       updatePigs color =
+--         calculateSurfaceLayer
+--           (pigLayer color)
+--           (surfaceLayer color)
+--           (pigLayer color)
+--           hmap
+--           dt
+--       !(newPigR, newSurR) = updatePigs Red
+--       !(newPigY, newSurY) = updatePigs Yellow
+--       !(newPigB, newSurB) = updatePigs Blue
+--       -- normalisedSurfaceLayers = normaliseSurfaceLayers (newSurR, newSurY, newSurB)
+--    in PigmentLayers
+--         { surfaceLayerDensitiesRYB = (newSurR, newSurY, newSurB) -- normalisedSurfaceLayers
+--         , pigmentDensitiesRYB = (newPigR, newPigY, newPigB)
+--         }
+
+-- stepPigments :: State -> Maybe Source -> Matrix Double -> VelocityField -> Double -> Int -> (ScalarField, ScalarField, ScalarField)
+-- stepPigments !prevState !mSrc !hmap !vstep !dt !n =
+--   let (r, y, b) = pigmentDensitiesRYB $ pigLayers prevState
+--       newPigmentLayer src color =
+--         updateDens
+--           (getPigmentLayer color prevState)
+--           src
+--           hmap
+--           vstep
+--           (mask prevState)
+--           dt
+--           n
+--       !defaultLayer = newPigmentLayer (zeroMatrix $ canvasDims prevState)
+--    in case mSrc of
+--         Nothing ->
+--           ( defaultLayer Red
+--           , defaultLayer Yellow
+--           , defaultLayer Blue)
+--         Just (src, color) ->
+--           case color of
+--             Red ->
+--               ( newPigmentLayer src Red
+--               , defaultLayer Yellow
+--               , defaultLayer Blue)
+--             Yellow ->
+--               ( defaultLayer Red
+--               , newPigmentLayer src Yellow
+--               , defaultLayer Blue)
+--             Blue ->
+--               ( defaultLayer Red
+--               , defaultLayer Yellow
+--               , newPigmentLayer src Blue)
+
+-- *** old pigment behaviour begin ***
 getSurfaceLayer :: Color -> State -> ScalarField
-getSurfaceLayer = getColorLayer surfaceLayerDensitiesRYB
-
-updateColorLayer ::
-     Color -> State -> ScalarField -> (ScalarField, ScalarField, ScalarField)
-updateColorLayer color state newVal =
-  let (r, y, b) = surfaceLayerDensitiesRYB $ pigLayers state
-   in case color of
-        Red    -> (newVal, y, b)
-        Yellow -> (r, newVal, b)
-        Blue   -> (r, y, newVal)
-
-updateSurfaceLayer :: Color -> State -> ScalarField -> PigmentLayers
-updateSurfaceLayer color prevState newVal =
-  let oldPigLayers = pigLayers prevState
-   in oldPigLayers
-        {surfaceLayerDensitiesRYB = updateColorLayer color prevState newVal}
-
-updatePigmentLayer :: Color -> State -> ScalarField -> PigmentLayers
-updatePigmentLayer color prevState newVal =
-  let oldPigLayers = pigLayers prevState
-   in oldPigLayers
-        {pigmentDensitiesRYB = updateColorLayer color prevState newVal}
-
-stepPigmentLayers :: State -> Matrix Double -> Double -> PigmentLayers
-stepPigmentLayers state hmap dt =
-  let pigLayer x = getPigmentLayer x state
-      surfaceLayer x = getSurfaceLayer x state
-      updatePigs color =
-        calculateSurfaceLayer
-          (pigLayer color)
-          (surfaceLayer color)
-          (pigLayer color)
-          hmap
-          dt
-      !(newPigR, newSurR) = updatePigs Red
-      !(newPigY, newSurY) = updatePigs Yellow
-      !(newPigB, newSurB) = updatePigs Blue
-      -- normalisedSurfaceLayers = normaliseSurfaceLayers (newSurR, newSurY, newSurB)
-   in PigmentLayers
-        { surfaceLayerDensitiesRYB = (newSurR, newSurY, newSurB) -- normalisedSurfaceLayers
-        , pigmentDensitiesRYB = (newPigR, newPigY, newPigB)
-        }
-
-stepPigments :: State -> Maybe Source -> Matrix Double -> VelocityField -> Double -> Int -> (ScalarField, ScalarField, ScalarField)
-stepPigments !prevState !mSrc !hmap !vstep !dt !n =
-  let (r, y, b) = pigmentDensitiesRYB $ pigLayers prevState
-      newPigmentLayer src color =
-        updateDens
-          (getPigmentLayer color prevState)
-          src
-          hmap
-          vstep
-          (mask prevState)
-          dt
-          n
-      !defaultLayer = newPigmentLayer (zeroMatrix $ canvasDims prevState)
-   in case mSrc of
-        Nothing ->
-          ( defaultLayer Red
-          , defaultLayer Yellow
-          , defaultLayer Blue)
-        Just (src, color) ->
-          case color of
-            Red ->
-              ( newPigmentLayer src Red
-              , defaultLayer Yellow
-              , defaultLayer Blue)
-            Yellow ->
-              ( defaultLayer Red
-              , newPigmentLayer src Yellow
-              , defaultLayer Blue)
-            Blue ->
-              ( defaultLayer Red
-              , defaultLayer Yellow
-              , newPigmentLayer src Blue)
+getSurfaceLayer _ = surfaceLayerDensity
+-- *** old pigment behaviour end ***
 
 ---- rest of the state
 data State = State
   { velocityField         :: !VelocityField -- the fluid velocity on top of the canvas. (varies)
   , waterDensity          :: !ScalarField -- the amount of water on top of the canvas. (varies)
-  , pigLayers             :: PigmentLayers -- the different pigment layers on the canvas (varies)
+  -- , pigLayers             :: !PigmentLayers -- the different pigment layers on the canvas (varies)
+  -- *** old pigment behaviour begin ***
+  , pigmentDensity        :: !ScalarField
+  , surfaceLayerDensity   :: !ScalarField
+  -- *** old pigment behaviour end ***
   , capillaryLayerDensity :: !ScalarField -- the density of water inside the capillary layer (varies)
   , heightMap             :: !ScalarField -- the heightmap (CONSTANT)
   , mask                  :: !(Matrix Bool) -- the boundaries where the stroke is respected (varies)
@@ -142,11 +151,15 @@ initialState sizeN =
    in State
         { velocityField = (initialMat, initialMat)
         , waterDensity = initialMat
-        , pigLayers =
-            PigmentLayers
-              { surfaceLayerDensitiesRYB = (initialMat, initialMat, initialMat)
-              , pigmentDensitiesRYB = (initialMat, initialMat, initialMat)
-              }
+        -- , pigLayers =
+        --     PigmentLayers
+        --       { surfaceLayerDensitiesRYB = (initialMat, initialMat, initialMat)
+        --       , pigmentDensitiesRYB = (initialMat, initialMat, initialMat)
+        --       }
+        -- *** old pigment behaviour begin ***
+        , pigmentDensity = initialMat
+        , surfaceLayerDensity = initialMat
+        -- *** old pigment behaviour end ***
         , capillaryLayerDensity = initialMat
         , heightMap = getWorleyNoise (genPoints 3 dims) dims
         , mask = matrixInit dims False
@@ -198,20 +211,26 @@ step mSrc !prevState
           (mask prevState)
           dt
           n
+      -- *** old pigment behaviour begin ***
+      (newPigmentLayer, newSurfaceLayer) =
+        calculateSurfaceLayer dField (surfaceLayerDensity prevState) dField (heightMap prevState) dt
+      -- *** old pigment behaviour end ***
       -- pigment updates
-      !sourceUpdatedPigLayer = (pigLayers prevState) { pigmentDensitiesRYB = stepPigments prevState mSrc hmap vstep dt n}
-      !newPigLayers =
-        stepPigmentLayers
-          (prevState {pigLayers = sourceUpdatedPigLayer})
-          hmap
-          dt
+      -- !sourceUpdatedPigLayer = (pigLayers prevState) { pigmentDensitiesRYB = stepPigments prevState mSrc hmap vstep dt n}
+
       -- capillary updates
       (newCapillary, newShallowFluid, newMask) =
         simulateCapillaryFlow capillary dField hmap n diff dt
    in prevState
         { waterDensity = dField
         , velocityField = vField
-        , pigLayers = newPigLayers
+        
+        -- *** old pigment behaviour begin ***
+        , pigmentDensity = newPigmentLayer
+        , surfaceLayerDensity = newSurfaceLayer
+        -- *** old pigment behaviour end ***
+
+        -- , pigLayers = sourceUpdatedPigLayer
         , capillaryLayerDensity = newCapillary
         , heightMap = heightMap prevState
         , mask = newMask
